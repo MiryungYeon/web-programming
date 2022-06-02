b5796b9d4730e9e289579619bcf39bd15bea6673
Miryung Yeon <74745946+MiryungYeon@users.noreply.github.com>
2022-06-02 15:34:20 +0900

    Delete myproject directory

--- myproject/config.py
+++
@@ -1,8 +0,0 @@
-import os # 디렉터리를 알기 위함
-
-BASE_DIR = os.path.dirname(__file__) # 대문자 변수는 환경변수 값이라고 생각, 잘 변하지 않는 초기 값
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
-SQLALCHEMY_TRACK_MODIFICATIONS = False # 이벤트 처리하는 옵션
-
-SECRET_KEY = "dev"
